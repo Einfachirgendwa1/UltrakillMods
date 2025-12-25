68be398f42a8a07fe27dc9e6ffc51115cb3a1e33
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2025-12-25 14:11:32 +0100

    Cleanup n shit

--- CoinMod/CoindMod.cs
+++ CoinMod/CoindMod.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using BepInEx;
 using HarmonyLib;
+using PluginConfig.API;
 using UltrakillMods.UnityWav;
 using UnityEngine;
 using static Common.Statics;
@@ -12,7 +13,14 @@ namespace UltrakillMods {
         private static AudioClip? audioClip;
         private readonly Harmony harmony = new Harmony("CoinMod");
 
+        private PluginConfigurator? config;
+
         private void Awake() {
+            string directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+
+            config = PluginConfigurator.Create("Coin Mod", "com.einfachirgendwa1.coinMod");
+            config.SetIconWithURL($"file://{Path.Combine(directory, "icon.png")}");
+
             byte[] sound;
             using (Stream stream = GetEmbeddedResource("ThrowCoinSound.wav")) {
                 sound = new byte[stream.Length];
--- build.py
+++ build.py
@@ -1,7 +1,7 @@
-from os import system, listdir
-from os.path import join, isdir, exists
-from shutil import copy, rmtree, copytree
-from subprocess import Popen
+import zipfile
+from os import system, listdir, walk
+from os.path import join, isdir, exists, relpath
+from shutil import copy
 
 base_dir = r"C:\Users\Think\AppData\Roaming\r2modmanPlus-local\ULTRAKILL\profiles\dev\BepInEx\plugins"
 
@@ -10,8 +10,18 @@ system("dotnet build")
 
 entries = []
 
+
+# noinspection PyTypeChecker
+def zip_directory(src: str, filename: str):
+    with zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in walk(src):
+            for file in files:
+                file_path = join(root, file)
+                relative_name = relpath(file_path, src)
+                zipf.write(file_path, relative_name)
+
+
 for entry in listdir():
-    print(f"Checking {entry}")
     if isdir(entry) and exists(join(entry, "BuildResult")):
         entries.append(entry)
 
@@ -20,13 +30,5 @@ for entry in listdir():
         copy(f"{entry}/obj/Debug/netstandard2.1/{entry}.dll", f"{entry}/BuildResult/{entry}")
         copy(f"{entry}/bin/Debug/netstandard2.1/Common.dll", f"{entry}/BuildResult/{entry}")
 
-        print(f"[{entry}] Deleting '{mod_dir}'")
-        rmtree(mod_dir, ignore_errors=True)
-
-        print(f"[{entry}] Copying new content to '{mod_dir}'")
-        copytree(f"{entry}/BuildResult", mod_dir)
-
-print(f"Installed: {entries}")
-
-print("Opening ULTRAKILL")
-Popen([r"C:\Program Files (x86)\Steam\steam.exe", "-applaunch", "1229490"])
+        print(f"[{entry}] Zipping directory")
+        zip_directory(f"{entry}/BuildResult", f"{mod_dir}.zip")
