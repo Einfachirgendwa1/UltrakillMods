5af06eda492b36dec3f291f8feb7ca355628c683
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2025-12-30 14:32:28 +0100

    messed around + versioning for VariantCustomizer

---
+++ MessingAround/BuildResult/README.md
@@ -0,0 +1,29 @@
+## Messing Around
+
+- this isnt a real mod
+- created by Einfachirgendwa1 and licensed under MIT
+
+### Compilation from Source
+
+#### Variant 1.
+
+Run the following command to create MessingAround.zip:
+
+```commandline
+python build_zip.py
+```
+
+You can then install the created MessingAround.zip by opening r2modman and navigate to Settings > Import local mod. Then
+just select MessingAround.zip to install.
+
+#### Variant 2.
+
+Create a r2modman profile called dev and manually install all dependencies. Then run:
+
+```commandline
+python dev.py
+```
+
+The mod will not show up in r2modman if installed via Variant 2.
+
+###### Guid: com.einfachirgendwa1.messingAround
---
+++ MessingAround/MessingAround.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using Common;
+using PluginConfig.API;
+using UnityEngine;
+
+namespace MessingAround {
+    [BepInPlugin("com.einfachirgendwa1.messingAround", "MessingAround", "1.0.0.0")]
+    public class MessingAround : BaseUnityPlugin {
+        private PluginConfigurator? config;
+        private GameObject? cur;
+        private GameObject? fugg;
+
+        private void Awake() {
+            string directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+
+            config = PluginConfigurator.Create("Messing Around", "com.einfachirgendwa1.messingAround");
+            config.SetIconWithURL($"file://{Path.Combine(directory, "icon.png")}");
+
+            AssetBundle assetBundle = AssetBundle.LoadFromFile(Path.Combine(directory, "fugg"));
+            Statics.Assert(assetBundle != null, () => "Failed to read asset bundle!");
+
+            fugg = assetBundle.LoadAsset<GameObject>("fugg");
+            Logger.LogInfo(fugg.name);
+            assetBundle.Unload(false);
+        }
+
+        private void Update() {
+            GunControl gunControl = GunControl.Instance;
+            if (gunControl == null) return;
+
+            GameObject currentWeapon = gunControl.currentWeapon;
+
+            bool isNew = cur != currentWeapon;
+            cur = currentWeapon;
+
+            if (currentWeapon == null || !isNew || currentWeapon.name != "Alternative Revolver Pierce(Clone)") {
+                return;
+            }
+
+            Transform? absRerigged = null;
+            foreach (Transform rerigged in currentWeapon.transform) {
+                absRerigged = rerigged;
+                foreach (Transform thing in rerigged.transform) {
+                    if (thing.name == "MinosRevolver_Cyllinder") thing.gameObject.SetActive(false);
+                }
+            }
+
+            GameObject fuggInstance = Instantiate(fugg!, absRerigged!, true);
+            fuggInstance.SetActive(true);
+            // fuggInstance.transform.position = new Vector3(0.73f, 1.04f, -0.337f);
+            // fuggInstance.transform.rotation = Quaternion.Euler(73.886f, -90.349f, -271.257f);
+            fuggInstance.transform.localScale = new Vector3(10, 10, 10);
+        }
+    }
+}
