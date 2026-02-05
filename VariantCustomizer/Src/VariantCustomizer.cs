6f1657d419901f9cab992a79024bce70a01c004b
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2026-02-05 18:44:44 +0100

    back to a single file

--- VariantCustomizer/Src/VariantCustomizer.cs
+++ VariantCustomizer/Src/VariantCustomizer.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using static Common.Statics;
 
+// ReSharper disable Unity.InefficientMultidimensionalArrayUsage
+
 namespace VariantCustomizer;
 
 [BepInPlugin("com.einfachirgendwa1.variantCustomizer", "VariantCustomizer", "1.1.0")]
@@ -31,15 +33,26 @@ public class VariantCustomizer : BaseUnityPlugin {
 
     private static GameObject? lastShop;
 
+    private static readonly string[] Guns = {
+        "Revolver",
+        "Shotgun",
+        "Nailgun",
+        "Railgun",
+        "Rocket Launcher"
+    };
+
     private readonly PluginConfigurator config = InitPluginConfig(
         "Variant Customizer",
         "com.einfachirgendwa1.variantCustomizer"
     );
 
+    private readonly GunColorPreset[,,] variants = new GunColorPreset[5, 2, 3];
+
     private BoolField? allowNotUnlocked;
+
+    private GunColorSetter[]? colorSetters;
     private GameObject? currentWeaponCache;
 
-    private GunSlot[] guns = { };
     private BoolField? modEnabled;
 
     private void Awake() {
@@ -52,25 +65,53 @@ public class VariantCustomizer : BaseUnityPlugin {
             true
         );
 
-        guns = new[] {
-            new GunSlot("Revolver", true, 1),
-            new GunSlot("Shotgun", true, 2),
-            new GunSlot("Nailgun", true, 3),
-            new GunSlot("Railgun", false, 4),
-            new GunSlot("Rocket Launcher", false, 5)
-        };
+        for (int gun = 0; gun < 5; gun++) {
+            string gunName = Guns[gun];
+            ConfigPanel slotPanel = new(config.rootPanel, gunName, gunName);
 
-        foreach (GunSlot gun in guns) {
-            gun.Subpanel(config.rootPanel);
-        }
+            if (!HasAlt(gun)) MakeConfig(gun, 0, slotPanel);
+            else {
+                ConfigPanel noAlt = new(slotPanel, $"Base {gunName}", $"{gunName}.base");
+                ConfigPanel yesAlt = new(slotPanel, $"Alternate {gunName}", $"{gunName}.alt");
 
-        modEnabled.onValueChange += _ => dirty = true;
-        allowNotUnlocked.onValueChange += data => {
-            dirty = true;
-            foreach (GunSlot gun in guns) {
-                gun.UpdateVisibility(data.value);
+                MakeConfig(gun, 0, noAlt);
+                MakeConfig(gun, 1, yesAlt);
             }
-        };
+
+            continue;
+
+            void MakeConfig(int thisGun, int alt, ConfigPanel parentPanel) {
+                for (int variant = 0; variant < 3; variant++) {
+                    int localVariant = variant;
+
+                    string variantName = variant switch {
+                        0 => "Blue Variation",
+                        1 => "Green Variation",
+                        2 => "Red Variation",
+                        _ => throw new ArgumentOutOfRangeException()
+                    };
+
+                    ConfigPanel variantPanel = new(parentPanel, variantName, $"{parentPanel.guid}.variant{variant}");
+
+                    ColorField color1 = new(variantPanel, "Color 1", variantPanel.guid + ".color1", Color.white);
+                    ColorField color2 = new(variantPanel, "Color 2", variantPanel.guid + ".color2", Color.white);
+                    ColorField color3 = new(variantPanel, "Color 3", variantPanel.guid + ".color3", Color.white);
+
+                    variants[gun, alt, variant] = new GunColorPreset(color1.value, color2.value, color3.value);
+
+                    color1.onValueChange += data => variants[thisGun, alt, localVariant].color1 = data.value;
+                    color2.onValueChange += data => variants[thisGun, alt, localVariant].color2 = data.value;
+                    color3.onValueChange += data => variants[thisGun, alt, localVariant].color3 = data.value;
+
+                    color1.onValueChange += _ => ApplyModColors();
+                    color2.onValueChange += _ => ApplyModColors();
+                    color3.onValueChange += _ => ApplyModColors();
+                }
+
+                modEnabled.onValueChange += _ => dirty = true;
+                allowNotUnlocked.onValueChange += _ => dirty = true;
+            }
+        }
     }
 
     private void Update() {
@@ -78,7 +119,8 @@ public class VariantCustomizer : BaseUnityPlugin {
             if (!ReferenceEquals(shop, lastShop)) {
                 lastShop = shop;
 
-                for (int g = 0; g < guns.Length; g++) {
+                colorSetters = shop.GetComponentsInChildren<GunColorSetter>();
+                for (int g = 0; g < 5; g++) {
                     GameObject window = shop.transform.Find(WeaponWindows[g]).gameObject
                                         ?? throw new Exception("Could not find window");
 
@@ -100,11 +142,22 @@ public class VariantCustomizer : BaseUnityPlugin {
         SkinnedMeshRenderer[] renderers = currentWeapon.GetComponentsInChildren<SkinnedMeshRenderer>();
 
         foreach (SkinnedMeshRenderer renderer in renderers) {
-            VariantColorId cid = new(gunControl.currentSlotIndex - 1, alternate,
-                gunControl.currentVariationIndex);
+            int slot = gunControl.currentSlotIndex - 1;
+            if (slot >= variants.GetLength(0) || slot < 0) {
+                Logger.LogWarning($"Gun index out of bounds ({slot} is >= {variants.GetLength(0)} or < 0)!");
+                continue;
+            }
 
-            GunColorPreset colors = cid.GetColors();
-            Logger.LogInfo($"got colors for {cid.ColorId("<num>")}: " + colors);
+            int variation = gunControl.currentVariationIndex;
+            if (variation >= variants.GetLength(2) || variation < 0) {
+                Logger.LogWarning(
+                    $"Variation index out of bounds ({variation} is >= {variants.GetLength(2)} or < 0)!");
+                continue;
+            }
+
+            GunColorPreset colors = variants[slot, alternate ? 1 : 0, variation];
+            Logger.LogInfo(
+                $"Colors of slot={slot}, alt={alternate}, variation={variation} is {colors.color1}, {colors.color2}, {colors.color3}");
 
             MaterialPropertyBlock block = new();
             renderer.GetPropertyBlock(block);
@@ -114,6 +167,38 @@ public class VariantCustomizer : BaseUnityPlugin {
             renderer.SetPropertyBlock(block);
         }
     }
+
+    private void ApplyModColors() {
+        dirty = true;
+        if (colorSetters is null) return;
+
+        for (int gun = 0; gun < variants.GetLength(0); gun++) {
+            for (int alt = 0; alt < variants.GetLength(1); alt++) {
+                for (int variant = 0; variant < variants.GetLength(2); variant++) {
+                    if (alt == 1 && !HasAlt(gun)) continue;
+
+                    GunColorPreset preset = variants[gun, alt, variant];
+                    Color[] colors = {
+                        preset.color1,
+                        preset.color2,
+                        preset.color3
+                    };
+
+                    for (int color = 0; color < colors.Length; color++) {
+                        PrefsManager.Instance.SetFloat($"{gun}.{color}.{(alt == 0 ? "" : "a")}r", colors[color].r);
+                        PrefsManager.Instance.SetFloat($"{gun}.{color}.{(alt == 0 ? "" : "a")}g", colors[color].g);
+                        PrefsManager.Instance.SetFloat($"{gun}.{color}.{(alt == 0 ? "" : "a")}b", colors[color].b);
+                        PrefsManager.Instance.SetFloat($"{gun}.{color}.{(alt == 0 ? "" : "a")}a", colors[color].a);
+                    }
+                }
+            }
+        }
+
+        Logger.LogInfo($"updating {colorSetters.Length} sliders");
+        colorSetters.ForEach(setter => setter.UpdateSliders());
+    }
+
+    private static bool HasAlt(int gun) => gun < 3;
 
     private void PatchShopTerminal(GameObject window, int gun) {
         GameObject variations = window.FindAssertExists(Logger, "Variation Screen", "Variations");
