8d824a5dca11f8fbbbfd65605dfd26d508aa9cf3
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2026-02-05 16:22:07 +0100

    everything still works

--- VariantCustomizer/Src/VariantCustomizer.cs
+++ VariantCustomizer/Src/VariantCustomizer.cs
@@ -1,8 +1,10 @@
-﻿using BepInEx;
-using Common;
+﻿using System;
+using BepInEx;
 using PluginConfig.API;
 using PluginConfig.API.Fields;
 using UnityEngine;
+using UnityEngine.UI;
+using static Common.Statics;
 
 namespace VariantCustomizer;
 
@@ -12,9 +14,20 @@ public class VariantCustomizer : BaseUnityPlugin {
     private static readonly int CustomColor2 = Shader.PropertyToID("_CustomColor2");
     private static readonly int CustomColor3 = Shader.PropertyToID("_CustomColor3");
 
+
+    private static readonly string[] WeaponWindows = {
+        "Revolver Window",
+        "Shotgun Window",
+        "Nailgun Window",
+        "Railcannon Window",
+        "Rocket Launcher Window"
+    };
+
     internal static bool Dirty;
 
-    private readonly PluginConfigurator config = Statics.InitPluginConfig(
+    private static GameObject? lastShop;
+
+    private readonly PluginConfigurator config = InitPluginConfig(
         "Variant Customizer",
         "com.einfachirgendwa1.variantCustomizer"
     );
@@ -57,16 +70,29 @@ public class VariantCustomizer : BaseUnityPlugin {
     }
 
     private void Update() {
+        if (Find(Logger, null, "Shop", "Canvas", "Background", "Main Panel", "Weapons") is { } shop) {
+            if (!ReferenceEquals(shop, lastShop)) {
+                lastShop = shop;
+
+                for (int g = 0; g < guns.Length; g++) {
+                    GameObject window = shop.transform.Find(WeaponWindows[g]).gameObject
+                                        ?? throw new Exception("Could not find window");
+
+                    PatchShopTerminal(window, g);
+                }
+            }
+        }
+
         GunControl gunControl = GunControl.Instance;
-        if (gunControl == null) return;
+        if (gunControl is null) return;
 
         GameObject currentWeapon = gunControl.currentWeapon;
-        if (currentWeapon == null || (currentWeapon == currentWeaponCache && !Dirty)) return;
+        if (currentWeapon is null || currentWeapon == currentWeaponCache && !Dirty) return;
         currentWeaponCache = currentWeapon;
         Dirty = false;
 
         GunColorGetter colorGetter = currentWeapon.GetComponentInChildren<GunColorGetter>();
-        bool alternate = colorGetter != null && colorGetter.altVersion;
+        bool alternate = colorGetter is not null && colorGetter.altVersion;
 
         Gun gun = guns[gunControl.currentSlotIndex - 1];
         bool useCustomColors = gun.UseCustomColors(allowNotUnlocked!.value) && modEnabled!.value;
@@ -83,6 +109,70 @@ public class VariantCustomizer : BaseUnityPlugin {
             block.SetColor(CustomColor2, colors.color2);
             block.SetColor(CustomColor3, colors.color3);
             renderer.SetPropertyBlock(block);
+        }
+    }
+
+    /// <summary>
+    ///     Deactivates the GameObject at "Variation Screen/Variations/Info and Color
+    ///     Panel/ColorButton".
+    ///     Deactivates everything at "Variation Screen/Variations/Variation Panel
+    ///     ([Blue/Green/Red])/Equipment/Buttons/*".
+    ///     Loads a ColorButton at every "Variation Screen/Variations/Variation Panel
+    ///     ([Blue/Green/Red])/Equipment"
+    /// </summary>
+    /// <param name="window">
+    ///     Should be a GameObject at "Shop/Canvas/Background/Main
+    ///     Panel/Weapons"
+    /// </param>
+    /// <param name="gun">The index of the gun</param>
+    /// <exception cref="NullReferenceException">
+    ///     Any of the GameObjects that should be
+    ///     deactivated don't exist
+    /// </exception>
+    private void PatchShopTerminal(GameObject window, int gun) {
+        GameObject variations = window.FindAssertExists(Logger, "Variation Screen", "Variations");
+        GameObject colorButton = variations.FindAssertExists(Logger, "Info and Color Panel", "ColorButton");
+
+        GameObject mainWindow = window.FindAssertExists(Logger, "Color Screen", "Main Window");
+        GunColorTypeGetter gctg = mainWindow.GetComponent<GunColorTypeGetter>();
+
+        colorButton.SetActive(false);
+
+        for (int i = 0; i <= 2; i++) {
+            string panelName = i switch {
+                0 => "Blue",
+                1 => "Green",
+                2 => "Red",
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            GameObject equipment = variations.FindAssertExists(
+                Logger,
+                $"Variation Panel ({panelName})",
+                "Equipment"
+            );
+
+            GameObject buttons = equipment.FindAssertExists(Logger, "Buttons");
+            foreach (Transform button in buttons.transform) {
+                button.gameObject.SetActive(false);
+            }
+
+            GameObject newColorButton = Instantiate(colorButton, buttons.transform, false);
+            newColorButton.SetActive(true);
+            newColorButton.transform.SetAsLastSibling();
+
+            int variant = i;
+            newColorButton
+                .GetComponent<Button>()
+                .Also(button => Assert(button is not null, () => "Color Button not found!"))
+                .onClick
+                .AddListener(() => {
+                        // TODO
+                    }
+                );
+
+
+            Canvas.ForceUpdateCanvases();
         }
     }
 }
